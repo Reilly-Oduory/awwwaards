993d03153c1b90539dc29fe3843be7f90d519b67
Reilly-Oduory <reilly.oduory@student.moringaschool.com>
2021-07-21 08:15:10 +0300

    models change

--- mysite/models.py
+++ mysite/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
@@ -7,10 +8,17 @@ class User(AbstractUser):
     pass
 
 class Profile(models.Model):
-    pass
+    bio = models.TextField()
+    d_o_b = models.DateField()
 
 class Project(models.Model):
-    pass
+    description = models.TextField()
+    slug = models.SlugField(max_length=15)
+    url = models.CharField(max_length=60)
+    user = models.ForeignKey(User, on_delete=CASCADE)
 
 class Review(models.Model):
-    pass
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    slug = models.SlugField(max_length=30)
+    user = models.ForeignKey(User, on_delete=CASCADE)
